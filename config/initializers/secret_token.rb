0fc92297cd38b5c4ea3e409750d321c0aff7e4e5
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-06 12:39:12 -0800

    added resources

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouterTester::Application.config.secret_token = '635da55224fce84241749d829a68f09ebfe81b2815de7c04796551ddb0c696598f7e7c5580a6e01749cda288d0ed09ad53e84796ab76c033a2519df57cc0b332'
